a1bf9099f95fa30970d0d836ed3b044575a15c2b
José Luis Rojas <126004067+ThePhraseologyCorner@users.noreply.github.com>
2023-12-07 13:26:53 -0500

    Add files via upload

---
+++ 19_spa_3t_v-contr-n.pl
@@ -0,0 +1,27 @@
+use strict;
+use locale;
+
+my ( @token, @lemma, @pos );
+
+while ( my $line = <STDIN> ){
+    chomp $line;
+
+    my @t = split ( /\t/, $line );
+    push ( @token, $t[0] );
+	push ( @lemma, $t[0] );
+    push ( @pos, $t[2] );
+}
+
+for ( my $i = 0 ; $i <= ($#token - 2) ; $i++ )
+{
+	if (
+			($pos[$i] eq "V")
+		and 
+			($pos[$i+1] eq "Contr")
+		and 
+			($pos[$i+2] eq "N")
+		)
+	{
+	print $token[$i], " ", $token[$i+1], " ", $token[$i+2], "\n";
+	}
+}
