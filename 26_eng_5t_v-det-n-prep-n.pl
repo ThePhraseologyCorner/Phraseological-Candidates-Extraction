fa4d73339b64088f032859ca4cdb5f205670f695
José Luis Rojas <126004067+ThePhraseologyCorner@users.noreply.github.com>
2023-12-07 14:03:47 -0500

    Add files via upload

---
+++ 26_eng_5t_v-det-n-prep-n.pl
@@ -0,0 +1,31 @@
+use strict;
+use locale;
+
+my ( @token, @lemma, @pos );
+
+while ( my $line = <STDIN> ){
+    chomp $line;
+
+    my @t = split ( /\t/, $line );
+    push ( @token, $t[0] );
+	push ( @lemma, $t[0] );
+    push ( @pos, $t[2] );
+}
+
+for ( my $i = 0 ; $i <= ($#token - 2) ; $i++ )
+{
+	if (
+			($pos[$i] eq "V")
+		and 
+			($pos[$i+1] eq "Det")
+		and 
+			($pos[$i+2] eq "N")
+		and 
+			($pos[$i+3] eq "Prep")
+		and 
+			($pos[$i+4] eq "N")
+		)
+	{
+	print $token[$i], " ", $token[$i+1], " ", $token[$i+2], " ", $token[$i+3], " ", $token[$i+4], "\n";
+	}
+}
